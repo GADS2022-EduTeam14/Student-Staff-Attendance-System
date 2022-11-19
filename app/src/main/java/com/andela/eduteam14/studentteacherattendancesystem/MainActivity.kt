787348d65c7f8da5fd28ba1d38a8cb75c711fa9c
khalidtouch <khalidhulabaloo9@gmail.com>
2022-11-19 14:53:39 +0100

    Push android project

---
+++ app/src/main/java/com/andela/eduteam14/studentteacherattendancesystem/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.andela.eduteam14.studentteacherattendancesystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
