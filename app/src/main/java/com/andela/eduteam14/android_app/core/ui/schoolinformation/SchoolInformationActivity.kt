9cf7cf45dc31479abe763c017aa6a188c9b85522
kj10dev <kj10dev@gmail.com>
2022-11-25 11:45:39 +0200

    4 Commit
    Extra UI + fragments

---
+++ app/src/main/java/com/andela/eduteam14/android_app/core/ui/schoolinformation/SchoolInformationActivity.kt
@@ -0,0 +1,11 @@
+package com.andela.eduteam14.android_app.core.ui.schoolinformation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SchoolInformationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_school_information)
+    }
+}
